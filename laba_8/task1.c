2a03e4c06bfdba16596aee50acb1dc610c2aeb1c
mmarinna007 <119120876+mmarinna007@users.noreply.github.com>
2023-06-23 12:41:37 +0300

    Update task1.c

--- laba_8/task1.c
+++ laba_8/task1.c
@@ -3,10 +3,11 @@
 #include <stdlib.h>
 
 int strend(char *s, char *t) {
+    
     int len_s = strlen(s);
     int len_t = strlen(t);
-    if (len_t >= len_s) {
-        for (int i = len_t - 1; i >= len_t-len_s; i--){
+    if (len_t <= len_s) {
+        for (int i = len_t - 1; i >= 0; i--){
             if (t[i] != s[i-len_t+len_s]) {
                 return 0;
             }
@@ -21,7 +22,7 @@ char* reading()
 {
   char *str = malloc(sizeof(char) * 11);
   int n = 0;
-  while (scanf("%10[^ \n]", str + 10*n) == 1) {
+  while (scanf("%10[^\n]", str + 10*n) == 1) {
     n++;
     str =  realloc(str, sizeof(char) * (10*n + 11));
   }
@@ -43,6 +44,6 @@ int main()
   int result = strend(s, t);
   free(s);
   free(t);
-  printf("%i", result);
+  printf("%i\n", result);
   
 }
